21a7f5b4d560e4c650fb03d3277bde28e9c349f6
Filip Brankiewicz <filip@guider-ai.com>
2022-10-21 14:31:29 +0200

    initial project structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,11 @@
+import Header from "components/Header/Header";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header headerLabel="Siema" isBig={true} />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,8 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-console.log("yes");
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
