16bddbafbf034ab4140ea69fba1e5c03d24280ff
Jakub Kesy <jakkes1@st.amu.edu.pl>
2022-10-21 14:11:49 +0200

    mnie dupa

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Dupa
+          Learn nie Dupa
         </a>
       </header>
     </div>
